c2476316fc7bddc56d7d7747906a5b6670496232
Rob Doyle <robdoyle@ymail.com>
2019-11-22 10:16:02 +0000

    deploy to heroku

---
+++ public/precache-manifest.441c90b6c16c0ced6b13a6dcad536431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cbd685279e015c6be3efb43331f531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dcd237a23b46a318ea2",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c87b80d9591628e3055b",
+    "url": "/static/js/2.180460ec.chunk.js"
+  },
+  {
+    "revision": "8dcd237a23b46a318ea2",
+    "url": "/static/js/main.29ebe2d9.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "6c6b377a7f8b29e22a5f8e4d82c1e2e2",
+    "url": "/static/media/logo.6c6b377a.png"
+  }
+]);
